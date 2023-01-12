8c2d4aec56355153d7f0e9363659ebc5bc3d4104
Jim Van Fleet <jim@jimvanfleet.com>
2023-01-11 19:03:21 -0500

    Adding some smarts to cluster deployment

--- bin/deploy-cluster.sh
+++ bin/deploy-cluster.sh
@@ -1,1 +1,5 @@
 kubectl apply -f deployment/
+
+kubectl wait --for=condition=ready pod --selector=app=postgres --timeout=90s
+
+scripts/run_db_command.sh
--- scripts/run_db_command.sh
+++ scripts/run_db_command.sh
@@ -1,12 +1,13 @@
 # Usage: pass in the DB container ID as the argument
+pod=`kubectl get pods --selector=app=postgres -o name`
 
 # Set database configurations
 export CT_DB_USERNAME=ct_admin
 export CT_DB_NAME=geoconnections
 
 
-cat ./db/2020-08-15_init-db.sql | kubectl exec -i $1 -- bash -c "psql -U $CT_DB_USERNAME -d $CT_DB_NAME"
+cat ./db/2020-08-15_init-db.sql | kubectl exec -i $pod -- bash -c "psql -U $CT_DB_USERNAME -d $CT_DB_NAME"
 
-cat ./db/udaconnect_public_person.sql | kubectl exec -i $1 -- bash -c "psql -U $CT_DB_USERNAME -d $CT_DB_NAME"
+cat ./db/udaconnect_public_person.sql | kubectl exec -i $pod -- bash -c "psql -U $CT_DB_USERNAME -d $CT_DB_NAME"
 
-cat ./db/udaconnect_public_location.sql | kubectl exec -i $1 -- bash -c "psql -U $CT_DB_USERNAME -d $CT_DB_NAME"
+cat ./db/udaconnect_public_location.sql | kubectl exec -i $pod -- bash -c "psql -U $CT_DB_USERNAME -d $CT_DB_NAME"
