72fac7e3359e04daa5ad75ca54850b4d1ba5d82b
Jim Van Fleet <jim@jimvanfleet.com>
2023-01-23 11:35:34 -0500

    Stopping place

--- modules/api/app/udaconnect/controllers.py
+++ modules/api/app/udaconnect/controllers.py
@@ -1,11 +1,10 @@
 from datetime import datetime
 
-from app.udaconnect.models import Location, Person
+from app.udaconnect.models import Person
 from app.udaconnect.schemas import (
-    LocationSchema,
     PersonSchema,
 )
-from app.udaconnect.services import LocationService, PersonService
+from app.udaconnect.services import PersonService
 from flask import request
 from flask_accepts import accepts, responds
 from flask_restx import Namespace, Resource
@@ -17,24 +16,6 @@ api = Namespace("UdaConnect", description="Connections via geolocation.")  # noq
 
 
 # TODO: This needs better exception handling
-
-
-@api.route("/locations")
-@api.route("/locations/<location_id>")
-@api.param("location_id", "Unique ID for a given Location", _in="query")
-class LocationResource(Resource):
-    @accepts(schema=LocationSchema)
-    @responds(schema=LocationSchema)
-    def post(self) -> Location:
-        request.get_json()
-        location: Location = LocationService.create(request.get_json())
-        return location
-
-    @responds(schema=LocationSchema)
-    def get(self, location_id) -> Location:
-        location: Location = LocationService.retrieve(location_id)
-        return location
-
 
 @api.route("/persons")
 class PersonsResource(Resource):
