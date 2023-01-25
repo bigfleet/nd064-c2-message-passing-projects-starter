bf979759b5b81582db827ee5966fff6bec1f1a28
Jim Van Fleet <jim@jimvanfleet.com>
2023-01-25 15:55:41 -0500

    Adding protobufs

--- modules/location_api/app/udaconnect/services.py
+++ modules/location_api/app/udaconnect/services.py
@@ -13,6 +13,9 @@ from app.udaconnect.schemas import LocationSchema
 from geoalchemy2.functions import ST_AsText, ST_Point
 from sqlalchemy.sql import text
 
+import location_pb2
+import location_pb2_grpc
+
 logging.basicConfig(level=logging.WARNING)
 logger = logging.getLogger("udaconnect-location-api")
 
@@ -37,9 +40,14 @@ class LocationService:
             raise Exception(f"Invalid payload: {validation_results}")
 
         logging.debug("location: %r", location)
-        # Turn order_data into a binary string for Kafka
-        kafka_data = json.dumps(location)
-        # Kafka producer has already been set up in Flask context
+        # Turn location data into a binary sequence for Kafka
+        kafka_data = location_pb2.LocationMessage(
+               person_id=int(location["person_id"]),
+               creation_time=location["creation_time"],
+               latitude=float(location["latitude"]),
+               longitude=float(location["longitude"])
+           ).SerializeToString()
+        
         logging.debug("data: %r", kafka_data)
         producer = KafkaProducer(bootstrap_servers=os.environ["FLASK_KAFKA_SERVER"])
         topic    = os.environ["FLASK_KAFKA_TOPIC"]
