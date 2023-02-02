9e9699f880d9e9f3b5de06f44006601943f6b757
Jim Van Fleet <jim@jimvanfleet.com>
2023-02-02 18:38:13 -0500

    Cluster liked this better

--- modules/location_consumer/consumer.py
+++ modules/location_consumer/consumer.py
@@ -7,11 +7,12 @@ from flask_kafka import FlaskKafka
 import psycopg2
 
 conn = psycopg2.connect(
-    host=os.environ["DB_HOST"],
-    port=os.environ["DB_PORT"],
-    database=os.environ["DB_NAME"],
-    user=os.environ["DB_USERNAME"],
-    password=os.environ["DB_PASSWORD"])
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT"),
+    database=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USERNAME"),
+    password=os.getenv("DB_PASSWORD")
+)
 
 app = Flask(__name__)
 
